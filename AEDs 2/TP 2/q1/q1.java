8f6f3044f1c7d14a142af2ec57a0e563b9632699
bernardogs04 <bernardogsaldanha04@gmail.com>
2026-09-14 21:51:11 -0300

    add tp2

--- AEDs 2/TP 2/q1/q1.java
+++ AEDs 2/TP 2/q1/q1.java
@@ -1,14 +1,14 @@
 class Veiculo{
 	private int id;
-	private string marca;
-	private string modelo;
+	private String marca;
+	private String modelo;
 	private int ano;
-	private string categoria;
-	private string[] combustivel;
+	private String categoria;
+	private String[] combustivel;
 	private int cilindros;
 	private double cilindrada;
-	private string transmissao;
-	private string tracao;
+	private String transmissao;
+	private String tracao;
 	private double consumoCidade;
 	private double consumoEstrada;
 	private double co2;
@@ -18,19 +18,19 @@ class Veiculo{
 	public int getId(){
 		return id;
 	}
-	public string getMarca(){
+	public String getMarca(){
 		return marca;
 	}
-	public string getModelo(){
+	public String getModelo(){
 		return modelo;
 	}
 	public int getAno(){
 		return ano;
 	}
-	public string getCategoria(){
+	public String getCategoria(){
 		return categoria;
 	}
-	public string[] getCombustivel(){
+	public String[] getCombustivel(){
 		return combustivel;
 	}
 	public int getCilindros(){
@@ -39,10 +39,10 @@ class Veiculo{
 	public double getCilindrada(){
 		return cilindrada;
 	}
-	public string getTransmissao(){
+	public String getTransmissao(){
 		return transmissao;
 	}
-	public string getTracao(){
+	public String getTracao(){
 		return tracao;
 	}
 	public double getConsumoCidade(){
@@ -60,6 +60,79 @@ class Veiculo{
 	public Data getDataRegistro(){
 		return dataRegistro;
 	}
+	public void setId(int a){
+		id = a;
+	}
+	public void setMarca(String a){
+		marca = a;
+	}
+	public void setModelo(String a){
+		modelo = a;
+	}
+	public void setAno(int a){
+		ano = a;
+	}
+	public void setCategoria(String a){
+		categoria = a;
+	}
+	public void setCombustivel(String a){
+		combustivel = a;
+	}
+	public void setCilindros(int a){
+		cilindros = a;
+	}
+	public void setCilindrada(double a){
+		cilindrada = a;
+	}
+	public void setTransmissao(String a){
+		transmissao = a;
+	}
+	public void setTracao(String a){
+		tracao = a;
+	}
+	public void setConsumoCidade(double a){
+		consumoCidade = a;
+	}
+	public void setConsumoEstrada(double a){
+		consumoEstrada = a;
+	}
+	public void setCo2(double a){
+		co2 = a;
+	}
+	public void setTurbo(boolean a){
+		turbo = a;
+	}
+	public Data getDataRegistro(data a){
+		dataRegistro = a;
+	}
+
+	public string format(){
+		int id = getId();
+		string marca = getMarca();
+		string modelo = getModelo();
+		int ano = getAno();
+		string categoria = getCategoria();
+		string[] combustivel = getCombustivel();
+		int cilindros = getCilindros();
+		double cilindrada = getCilindrada();
+		string transmissao = getTransmissao();
+		string tracao = getTracao();
+		double consumoCidade = getConsumoCidade();
+		double consumoEstrada = getConsumoEstada();
+		double co2 = getCo2();
+		boolean turbo = getTurbo();
+		Data dataRegistro = getDataRegistro;
+
+		String combustivelStr = "[";
+    		for (int i = 0; i < combustivel.length; i++) {
+        		combustivelStr += combustivel[i];
+        		if (i < combustivel.length - 1) {
+            			combustivelStr += ", ";
+        			}
+    			}
+    		combustivelStr += "]";
+		return String.format ("[%d ## %s ## %s ## %d ## %s ## %s ## %d ## %s ## %s ## %s ## %s ## %s ## %s ## %b ## %s]",id, marca, modelo, ano, categoria, Arrays.toString(combustivel), cilindros, cilindrada, transmissao, tracao, consumoCidade, consumoEstrada, co2, turbo, dataRegistro)
+	}
 
 }
 
@@ -67,6 +140,25 @@ class Data{
 	private int ano;
 	private int mes;
 	private int dia;
+
+	public int getAno(){
+		return ano;
+	}
+	public int getMes(){
+		return mes;
+	}
+	public int getDia(){
+		return dia;
+	}
+	public int setAno(int a){
+		ano = a;
+	}
+	public int setMes(int a){
+		mes = a;
+	}
+	public int setDia(int a){
+		dia = a;
+	}
 }
 
 class LeitorCsv{
